65f771b1620dffa8b07dddf12fd4c13c03b849b4
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2020-12-19 12:26:22 -0500

    get info about decorator using reflect-metadata

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
+import 'reflect-metadata';
 import TestClass from './src/test';
 
 function main() {
-    let start = new Date();
     let instance = new TestClass();
-    console.log("Got instance", instance)
-    
+
+    console.log("befire route_1", instance)
+    instance.route_1();
+    console.log("after route_1", instance)
+
+    // https://stackoverflow.com/questions/51170130/check-if-property-is-decorated-with-specific-annotation-typescript
+    /** Loop through each function in Class
+     *      if function is using middleware decorator (see below)
+     *          run the function
+     * 
+     */
+    console.log('using middleware', !!Reflect.getMetadata('MiddlewareDecorator', instance, 'route_1'))
 }
 
 main();
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,39 @@
-export default class Test {
+import 'reflect-metadata';
+
+const middlewareKey = 'MiddlewareDecorator'
+
+const middleware = (middlewareToAdd?: any[]) => {
+    return function(target: any /** Object */, propertyKey: string, descriptor: PropertyDescriptor) {
+        Reflect.defineMetadata(middlewareKey, true, target, propertyKey);
+        
+        let originalMethod = descriptor.value;
+
+        descriptor.value = function(...args) {
+            /** Set appropriate middleware the first time around! */
+            if(!this.__APPLIED_MIDDLEWARE) {
+                this.__MIDDLEWARE = middlewareToAdd;
+                this.__APPLIED_MIDDLEWARE = true;
+
+                return;
+            }
+
+            originalMethod.apply(this, args);
+        }
+        return descriptor;
+}}
+
+const buildController = (target: any): any => {
+    return class extends target {
+        /** DO NOT MODIFY! */
+        __IS_CONTROLLER = true
+        __APPLIED_MIDDLEWARE = false;
+
+        __MIDDLEWARE = [];
+    }
+}
+
+class Test {
+    @middleware(['middleware_1', 'middleware_2'])
     route_1(req: any, res: any, next: () => {}) {
         return `Got parameters: ${req}, ${res}, ${next}.`
     }
@@ -12,3 +47,5 @@ export default class Test {
         console.log("Called helper!")
     }
 }
+
+export default buildController(Test)
