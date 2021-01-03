e48774df1c3a36f9350c5c35d5167f5033f4d69d
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-01-03 12:08:14 -0500

    update objects available in controller for intellisense

--- src/BaseController.ts
+++ src/BaseController.ts
@@ -1,17 +1,28 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
+/** 
+ * 
+ * This class **does not** implement any functionality, 
+ * extend this for intellisense with your editor 
+ * of choice!
+ *  
+ */
 export abstract class BaseController {
     /** The root path for the controller.
      * If not specified, will default to the root '/'.
      */
     static PATH: string = '/';
 
+    req: Request;
+    res: Response;
+
     request: Request;
     response: Response;
     next: NextFunction;
-    params: any;
 
-    helpers: any;
+    locals: any;
+    params: any;
+    query: any;
     
     [x: string]: any
 }
