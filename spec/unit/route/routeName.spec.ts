1a8f1399d38ef833c8e8303caf23c2b99c0f4fad
Bilal Ahmed <bill.ahmed@mail.utoronto.ca>
2021-01-03 11:54:42 -0500

    add test cases for url wildcard parameters

--- spec/unit/route/routeName.spec.ts
+++ spec/unit/route/routeName.spec.ts
@@ -8,7 +8,7 @@ import buildController from '../../../src/utils/buildController';
 
 const app = express();
 
-test('uses function name as route path', async t => {
+test('it uses function name as route path', async t => {
     class NoNameOverride {
         @route()
         async endpointName(req, res) { res.sendStatus(200); }
@@ -21,7 +21,7 @@ test('uses function name as route path', async t => {
     t.is(resp.ok, true);
 })
 
-test('uses given name as route path', async t => {
+test('it uses given name as route path', async t => {
     class NoNameOverride {
         @route({ name: 'differentName' })
         async endpointName(req, res) { res.sendStatus(200); }
@@ -37,7 +37,7 @@ test('uses given name as route path', async t => {
     t.is(resp_2.ok, false);
 })
 
-test('handle leading / character', async t => {
+test('it handles leading / character', async t => {
     class NoNameOverride {
         @route({ name: '/differentName' })         /** Route name starts with '/' character, should still work! */
         async endpointName(req, res) { res.sendStatus(200); }
@@ -52,3 +52,46 @@ test('handle leading / character', async t => {
     t.is(resp.ok, true);
     t.is(resp_2.ok, false);
 })
+
+test('it handles parameters in URL', async t => {
+    class Paramter {
+        @route('/:id')         /** Route name starts with '/' character, should still work! */
+        async index(req, res) { res.sendStatus(200); }
+
+        @route(':id')
+        async another(req, res) { res.sendStatus(200); }
+    }
+
+    let router = buildController(Paramter, { path: '/route4' })
+    app.use(router)
+
+    let resp = await request(app).get('/route4/index/first_id').expect(200);
+    let resp_2 = await request(app).get('/route4/index/another_id').expect(200);
+    let resp_3 = await request(app).get('/route4/index').expect(404);
+
+    let resp_4 = await request(app).get('/route4/another/another_id').expect(200);
+
+    t.is(resp.ok, true);
+    t.is(resp_2.ok, true);
+    t.is(resp_3.ok, false);
+    t.is(resp_4.ok, true);
+});
+
+test('it handles nested paramters in URL', async t => {
+    class MultipleParamter {
+        @route('/:organization/:id')         /** Route name starts with '/' character, should still work! */
+        async one(req, res) { res.sendStatus(200); }
+
+        @route('/:organization/:first_id/:another_id')
+        async two(req, res) { res.sendStatus(200); }
+    }
+
+    let router = buildController(MultipleParamter, { path: '/route5' })
+    app.use(router)
+
+    let resp = await request(app).get('/route5/one/microsoft/1234').expect(200);
+    let resp_2 = await request(app).get('/route5/two/apple/5555/8765').expect(200);
+
+    t.is(resp.ok, true);
+    t.is(resp_2.ok, true);
+});
